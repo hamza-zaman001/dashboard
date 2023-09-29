f1c5873ac0814a5d7671520ecd1a16c840bebd5c
hamza-zaman001 <hamzabadiuzzamankhan@gmail.com>
2023-09-30 00:32:48 +0500

    .

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Cutomers = () => {
+const Customers = () => {
   return (
     <div>
       Cutomers
@@ -8,4 +8,4 @@ const Cutomers = () => {
   )
 }
 
-export default Cutomers
+export default Customers
